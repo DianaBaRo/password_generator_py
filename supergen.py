fa02afff42d28e92898a83a81cf2daef8f8d77d6
Diana Badas <diana.badas@gmail.com>
2020-02-17 13:01:59 +0000

    Create generateand password_check

--- supergen.py
+++ supergen.py
@@ -1,1 +1,44 @@
+#import bcrypt
+#import time
+import hashlib
+from urllib.parse import urlparse
+import re
 
+def generate(master, url, length=10, algorithm='md5'):
+
+    #.encode to prevent typeError : Unicode-objects must be encoded before hashing
+    password = (master + ":" + url).encode('utf-8')
+
+    #salt = bcrypt.gensalt(rounds=16)
+    #hashed = bcrypt.hashpw(password, salt)
+
+    count = 0
+    
+    hashed = hashlib.md5(password).hexdigest()
+
+    return hashed[:length]
+    
+
+def password_check(password):
+    reg = """^[a-z]                          # start with lowercase
+                   [a-zA-Z0-9]*                    # stuff
+                   (?:(?:[A-Z][a-zA-Z0-9]*[0-9])|  # uppercase stuff number OR
+                   (?:[0-9][a-zA-Z0-9]*[A-Z]))     # number stuff uppercase
+                   [a-zA-Z0-9]*$"""
+
+    #compiling regex
+    pat = re.compile(reg)
+
+    #searching regex
+    mat = re.search(pat, password)
+
+    #validating conditions
+    if mat:
+        True
+        print("Password OK")
+    else:
+        False
+        print("Password not OK")
+
+generate("ola", "kease", 10)
+password_check("aywFd0bmnJ")
